b0b2020b043a3be58d13021430e5a20a050caa0c
sergiomaddaleno <madalenofi@esat-alumni.com>
2023-10-14 21:21:41 +0200

    Imgui rework main menu
    With the namespace added before, now the window doesnt change when clicking a button.

--- src/interface.cc
+++ src/interface.cc
@@ -4,34 +4,54 @@
 #include "../include/interface.h"
 #include "variables.cc"
 
-void MainMenu(){
+void MainMenu() {
 
-    esat::DrawSetTextFont("./resources/font/Arka_solid.ttf");
-	esat::DrawSetFillColor(255, 255, 255);
-    esat::DrawSetTextSize(120.0f);
-    esat::DrawText(260, 200, "Breakout");
-    
-    ImVec2 windowSize(500, 350);
-    ImGui::SetNextWindowPos(ImVec2((kWindowWidth - windowSize.x) * 0.5f, (kWindowHeight - windowSize.y) * 0.5f + 50));
+    float fontSize = ImGui::GetFontSize();
 
-    ImGui::Begin("                                 Menu", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+    ImVec2 windowSize(600, 500);
+    ImVec2 windowPos(
+        (kWindowWidth - windowSize.x) * 0.5f,
+        (kWindowHeight - windowSize.y) * 0.5f
+    );
 
-    ImVec2 rect1Pos((windowSize.x - 100) * 0.5f, (windowSize.y - 50) * 0.4f);
-    ImVec2 rect2Pos((windowSize.x - 100) * 0.5f, (windowSize.y - 50) * 0.4f + 70);
+    ImGui::SetNextWindowPos(windowPos);
+    ImGui::SetNextWindowSize(windowSize);
 
-    ImGui::SetCursorPos(rect1Pos);
-    if (ImGui::Button("Start Game", ImVec2(100, 40))) {
+    ImGui::SetNextWindowBgAlpha(0.0f);
+
+    ImGui::Begin("Main Menu", NULL,
+        ImGuiWindowFlags_NoTitleBar | 
+        ImGuiWindowFlags_NoResize |   
+        ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoSavedSettings); 
+
+    ImVec2 buttonSize(windowSize.x, 120);
+
+    ImGui::Dummy(ImVec2(0, 30.0f));
+
+    ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(0, 89, 79, 255));
+
+    if (ImGui::Button("Start Game", buttonSize)) {
         window_type = TWindowType::start_game;
     }
 
-    ImGui::SetCursorPos(rect2Pos);
-    if(ImGui::Button("Quit", ImVec2(100, 40))) {
+    //Spacing between buttons
+    ImGui::Dummy(ImVec2(0, 180.0f));
+
+    if (ImGui::Button("Quit", buttonSize)) {
         window_type = TWindowType::quit_game;
     }
 
+    ImGui::PopStyleColor();
+
     ImGui::End();
-    ImGui::Render();
 }
+
+
+
+
+
+
 
 
 
